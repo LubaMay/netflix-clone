bba3c06a2e8a3e27bd13c3e886524bf9da794114
Lubov May <liubov.mayorova@gmail.com>
2021-10-28 17:24:26 -0700

    Created Footer Compound Component - with styled components Container, Row, Column, Link, Title, Break. Created styles for styled components. Created Footer Container. Created GlobalStyles fo an app.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,12 @@
 import React from "react";
-import Jumbotron from "../components/jumbotron";
 import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
 
 export default function Home() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
